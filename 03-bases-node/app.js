ba8dd4d835ae88aa3724fd67f7154d402628eb28
EricQuinterX <eric.brian.quinteros@gmail.com>
2020-01-13 07:38:45 -0300

    configuracion del yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,30 +1,25 @@
-const argv = require('yargs')
-        .command('listar', 'Imprime en consola la tabla de multiplicar', {
-            base: {
-                demand: true,
-                alias: 'b'
-            },
-            limite: {
-                alias: 'l',
-                default: 10
-            }
-        })
-        .help()
-        .argv;
 
-const { crearArchivo } = require('./multiplicar/multiplicar');
+const argv = require('./config/yargs').argv;
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
-console.log('Limite', argv.limite);
+let comando = argv._[0];
 
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Se creo el archivo ${archivo}`))
+            .catch(err => console.log(err));
+        break;
+    default:
+        console.log('Comando no reconocido');
+}
 
 // let parametro = argv[2];
 // let base = parametro.split('=')[1];
 
 // console.log(process.argv);
 // console.log(base * 2);
-
-// crearArchivo(base)
-//     .then(archivo => console.log(`Se creo el archivo ${archivo}`))
-//     .catch(err => console.log(err));
-
 
